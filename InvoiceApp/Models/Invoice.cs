9422e5c12e954dbc4e2a88b04f5d058bf6b9c81e
Gift <direodirile95@gmail.com>
2025-01-31 21:33:15 +0200

    created applicaitondatabasecontext. left the invoice class unfinished incomplete property on the class.

--- InvoiceApp/Models/Invoice.cs
+++ InvoiceApp/Models/Invoice.cs
@@ -2,5 +2,17 @@
 {
     public class Invoice
     {
+        public int Id { get; set; }
+
+        public string Number { get; set; } = "";
+        public string Status { get; set; } = "";
+        public DateOnly? IssueDate { get; set; }
+        //Dateonly represents only date without time
+        //? makes it nullable ,allowing it to hold nulll when value is not assigned
+        //so basically the question mark allows the user to leave the input empty as sometimes the value type can be undefined.
+
+        public DateOnly? Duedate { get; set; }
+
+
     }
 }
