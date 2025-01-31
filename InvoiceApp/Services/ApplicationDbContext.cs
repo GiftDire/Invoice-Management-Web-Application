49592ef02c09fc007e272ab19e323683e3b60aac
Gift <direodirile95@gmail.com>
2025-01-31 21:22:24 +0200

    Add project files.

---
+++ InvoiceApp/Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+//this class and package above is gonna  help us connect to our database
+
+namespace InvoiceApp.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
