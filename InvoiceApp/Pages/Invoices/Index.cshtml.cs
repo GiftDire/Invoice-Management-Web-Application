d521ba7ec30016219ab2221592f8a81372107d21
Gift <direodirile95@gmail.com>
2025-02-02 12:41:57 +0200

    updated the code of the page

--- InvoiceApp/Pages/Invoices/Index.cshtml.cs
+++ InvoiceApp/Pages/Invoices/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using InvoiceApp.Models;
 using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,12 +9,15 @@ namespace InvoiceApp.Pages.Invoices
     {
         private readonly ApplicationDbContext context;
 
+        public List<Invoice> invoicelist = new List<Invoice>();
+
         public IndexModel(ApplicationDbContext context)
         {
             this.context = context;
         }
         public void OnGet()
         {
+            invoicelist = context.Invoices.OrderByDescending(i =>i.Id).ToList();
         }
     }
 }
