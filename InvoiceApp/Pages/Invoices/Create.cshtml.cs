aeb035901b17cf695d33c327e80e27c76e358595
Gift <direodirile95@gmail.com>
2025-02-04 11:35:54 +0200

    Worked on the saving the data that is coming from the user but the feature is not currenly working it needs some debugging

--- InvoiceApp/Pages/Invoices/Create.cshtml.cs
+++ InvoiceApp/Pages/Invoices/Create.cshtml.cs
@@ -1,4 +1,5 @@
 using InvoiceApp.Models;
+using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,10 +7,54 @@ namespace InvoiceApp.Pages.Invoices
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext context;
+
         [BindProperty]
         public InvoiceDto InvoiceDto { get; set; } = new();
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        //requesting the applicationdb context from the service container using a contructor
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost() 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return Page();
+            
+            
+            }
+
+            var invoice = new Invoice
+            {
+                Number = InvoiceDto.Number,
+                Status = InvoiceDto.Status,
+                IssueDate = InvoiceDto.IssueDate,
+                Duedate = InvoiceDto.Duedate,
+
+                service = InvoiceDto.service,
+                UnirPrice = InvoiceDto.UnirPrice,
+                Quantity = InvoiceDto.Quantity,
+
+                Clientname = InvoiceDto.Clientname,
+                Email = InvoiceDto.Email,
+                Phone = InvoiceDto.Phone,
+                Address = InvoiceDto.Address,
+
+
+            };
+            context.Invoices.Add(invoice);//saving the variable to the database
+            context.SaveChanges();//save the modification
+
+            //redirecting the user to the invoice page
+
+            return RedirectToPage("/Invoices/Index");
+        
         }
     }
 }
