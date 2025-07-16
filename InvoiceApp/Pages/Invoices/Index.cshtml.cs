564b827e1153b51f9365c3921fc8b069955a5b20
Gift <direodirile95@gmail.com>
2025-07-16 17:33:11 +0200

    Today i added new functions which is the search function that is goinf to allow the use to seach though their invoces and i also added an action button that allows the user to click on once the the client  have paid for the services rendered

--- InvoiceApp/Pages/Invoices/Index.cshtml.cs
+++ InvoiceApp/Pages/Invoices/Index.cshtml.cs
@@ -2,6 +2,8 @@ using InvoiceApp.Models;
 using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;//adding the EF.functions.like
 
 namespace InvoiceApp.Pages.Invoices
 {
@@ -15,9 +17,36 @@ namespace InvoiceApp.Pages.Invoices
         {
             this.context = context;
         }
+        //bind search item for the query string 
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; } 
         public void OnGet()
         {
             invoicelist = context.Invoices.OrderByDescending(i =>i.Id).ToList();
+
+            var q = context.Invoices.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                q =q.Where(i => EF.Functions.Like(i.Number, $"%{SearchTerm}%") ||
+                                EF.Functions.Like(i.Clientname, $"%{SearchTerm}%") ||
+                                EF.Functions.Like(i.Email, $"%{SearchTerm}%") ||
+                                EF.Functions.Like(i.Phone, $"%{SearchTerm}%") ||
+                                EF.Functions.Like(i.Address, $"%{SearchTerm}%"));
+            }
+            invoicelist = q.OrderByDescending(i => i.Id).ToList();
         }
+        
+        public IActionResult OnPostMarkPaid(int id)
+        {
+            var inv = context.Invoices.Find(id);
+            if(inv != null && inv.Status != "Paid")
+            {
+                inv.Status = "Paid";
+                context.SaveChanges();
+            }
+            return RedirectToPage(new { SearchTerm });
+        }
+        
     }
 }
